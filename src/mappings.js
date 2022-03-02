d4d0c73114ab91099c89e6883434875b25cf5854
Kshitiz Agrawal <kshitizagrawal@outlook.com>
2022-03-03 03:26:25 +0530

    add mappings for products

---
+++ src/mappings.js
@@ -0,0 +1,40 @@
+export const catMap = {
+    "bathroom and kitchen fittings": "621fd672e4142b86ed0c7be2",
+};
+
+export const subCatMap = {
+    "sanitary": "621fd68be4142b86ed0c7be5",
+};
+
+export const grpMap = {
+    "faucet": "621fd6a7e4142b86ed0c7be9",
+    "shower": "621fd723e4142b86ed0c7c00",
+    "Basin": "621fd7c9e4142b86ed0c7c14",
+    "accessories": "621fd8b6e4142b86ed0c7c37",
+    "fittings": "621fd90de4142b86ed0c7c46",
+};
+
+export const subGrpMap = {
+    "single lever faucet": "621fd6bae4142b86ed0c7bee",
+    "quarter turn faucet": "621fd6c0e4142b86ed0c7bf0",
+    "half turn faucet": "621fd6c7e4142b86ed0c7bf2",
+    "sensor & touch faucets": "621fd6cee4142b86ed0c7bf4",
+    "push taps": "621fd6d3e4142b86ed0c7bf6",
+    "swan neck": "621fd6d8e4142b86ed0c7bf8",
+    "bathtub faucet": "621fd6dce4142b86ed0c7bfa",
+    "overhead shower": "621fd73be4142b86ed0c7c05",
+    "hand shower": "621fd741e4142b86ed0c7c07",
+    "shower coupling": "621fd749e4142b86ed0c7c09",
+    "shower arm": "621fd74ee4142b86ed0c7c0b",
+    "shower accessories": "621fd753e4142b86ed0c7c0d",
+    "kitchen sink": "621fd7dde4142b86ed0c7c19",
+    "pedestals": "621fd7e1e4142b86ed0c7c1b",
+    "wash basin": "621fd7e5e4142b86ed0c7c1d",
+    "urinal accessories": "621fd8e6e4142b86ed0c7c3f",
+    "bath accessories": "621fd8f5e4142b86ed0c7c41",
+    "flush valve": "621fd91ee4142b86ed0c7c4b",
+    "angled valve": "621fd925e4142b86ed0c7c4d",
+    "bottle traps": "621fd929e4142b86ed0c7c4f",
+    "waste coupling": "621fd92fe4142b86ed0c7c51",
+    "other auxillaries": "621fd933e4142b86ed0c7c53",
+}
